6c68b935d31c865a06e8b127d1b574a0478b03ab
dpflaalee <dpflaalee@naver.com>
2025-06-05 18:22:54 +0900

    250605

--- front/reducers/group.js
+++ front/reducers/group.js
@@ -1,6 +1,7 @@
 import produce from 'immer';
 
-// Action Types
+//-------------- Action Types---------------//
+
 //그룹 리스트 로드
 export const LOAD_GROUPS_REQUEST = 'LOAD_GROUPS_REQUEST';
 export const LOAD_GROUPS_SUCCESS = 'LOAD_GROUPS_SUCCESS';
@@ -48,25 +49,28 @@ export const LOAD_JOIN_REQUESTS_FAILURE = 'LOAD_JOIN_REQUESTS_FAILURE';
 
 //승인
 export const APPROVE_JOIN_REQUEST = 'APPROVE_JOIN_REQUEST';
+export const APPROVE_JOIN_SUCCESS = 'APPROVE_JOIN_SUCCESS';
+export const APPROVE_JOIN_FAILURE = 'APPROVE_JOIN_FAILURE';
 //거절
 export const REJECT_JOIN_REQUEST = 'REJECT_JOIN_REQUEST';
+export const REJECT_JOIN_SUCCESS = 'REJECT_JOIN_SUCCESS';
+export const REJECT_JOIN_FAILURE = 'REJECT_JOIN_FAILURE';
+
+//-------------- 초기값---------------//
 
 export const initialState = {
   // 그룹 목록 불러오기
   loadGroupsLoading: false,
   loadGroupsDone: false,
   loadGroupsError: null,
-
   // 그룹 생성
   createGroupLoading: false,
   createGroupDone: false,
   createGroupError: null,
-
   // 그룹 수정
   updateGroupLoading: false,
   updateGroupDone: false,
   updateGroupError: null,
-
   // 그룹 삭제
   deleteGroupLoading: false,
   deleteGroupDone: false,
@@ -91,17 +95,14 @@ export const initialState = {
   applyGroupLoading: false,
   applyGroupDone: false,
   applyGroupError: null,
-
   // 가입 요청 불러오기 (방장용)
   loadJoinRequestsLoading: false,
   loadJoinRequestsDone: false,
   loadJoinRequestsError: null,
-
   // 가입 요청 승인
   approveJoinRequestLoading: false,
   approveJoinRequestDone: false,
   approveJoinRequestError: null,
-
   // 가입 요청 거절
   rejectJoinRequestLoading: false,
   rejectJoinRequestDone: false,
@@ -112,93 +113,186 @@ export const initialState = {
   joinRequests: [],  // 가입 요청 목록
 };
 
-const reducer = (state = initialState, action) =>
-  produce(state, (draft) => {
+//-------------- next---------------//
+
+const reducer = (state = initialState, action) => produce(state, (draft) => {
     switch (action.type) {
+//------------------- 그룹관리 -------------------//
+      //그룹불러오기
       case LOAD_GROUPS_REQUEST:
-      case CREATE_GROUP_REQUEST:
-      case UPDATE_GROUP_REQUEST:
-      case DELETE_GROUP_REQUEST:
-      case LOAD_MEMBERS_REQUEST:
-      case KICK_MEMBER_REQUEST:
-      case TRANSFER_OWNERSHIP_REQUEST:
-      case APPLY_GROUP_REQUEST:
-      case LOAD_JOIN_REQUESTS_REQUEST:
-        draft.isLoading = true;
-        draft.error = null;
+        draft.loadGroupsLoading = true;
+        draft.loadGroupsDone = false;
+        draft.loadGroupsError = null;
         break;
-
       case LOAD_GROUPS_SUCCESS:
+        draft.loadGroupsLoading = false;
         draft.groups = action.data;
-        draft.isLoading = false;
+        draft.loadGroupsDone = true;
         break;
-
+      case LOAD_GROUPS_FAILURE:
+        draft.loadGroupsLoading = false;
+        draft.loadGroupsError = action.error;
+        break;       
+      //그룹생성
+      case CREATE_GROUP_REQUEST:
+        draft.createGroupLoading = true;
+        draft.createGroupDone = false;
+        draft.createGroupError = null;
+        break;
       case CREATE_GROUP_SUCCESS:
+        draft.createGroupLoading = false;
         draft.groups.unshift(action.data);
-        draft.isLoading = false;
+        draft.createGroupDone=true;
         break;
-
+      case CREATE_GROUP_FAILURE:
+        draft.createGroupLoading = false;
+        draft.createGroupError = action.error; 
+        break;       
+      //그룹 업데이트
+      case UPDATE_GROUP_REQUEST:
+        draft.updateGroupLoading = true;
+        draft.updateGroupDone = false;
+        draft.updateGroupError = null;
+        break;
       case UPDATE_GROUP_SUCCESS:
+        draft.updateGroupLoading = false;
         draft.groups = draft.groups.map((group) =>
           group.id === action.data.id ? action.data : group
         );
-        draft.isLoading = false;
+        draft.updateGroupDone=true;
         break;
-
+      case UPDATE_GROUP_FAILURE:
+        draft.updateGroupLoading = false;
+        draft.updateGroupError = action.error;
+        break;
+      //그룹삭제
+      case DELETE_GROUP_REQUEST:
+        draft.deleteGroupLoading = true;
+        draft.deleteGroupDone = false;
+        draft.deleteGroupError = null;
+        break;        
       case DELETE_GROUP_SUCCESS:
+        draft.deleteGroupLoading = false;
         draft.groups = draft.groups.filter(
           (group) => group.id !== action.data
         );
-        draft.isLoading = false;
+        draft.deleteGroupDone=true;
+        break;
+      case DELETE_GROUP_FAILURE:
+        draft.deleteGroupLoading = false;
+        draft.deleteGroupError = action.error;
         break;
 
+//------------------- 멤버관리 -------------------//
+      //멤버 불러오기
+      case LOAD_MEMBERS_REQUEST:
+        draft.loadMembersLoading = true;
+        draft.loadMembersDone = false;
+        draft.loadMembersError = null;
+        break;
       case LOAD_MEMBERS_SUCCESS:
+        draft.loadMembersLoading = false;
         draft.members = action.data;
-        draft.isLoading = false;
+        draft.loadMembersDone = true;
         break;
-
+      case LOAD_MEMBERS_FAILURE:
+        draft.loadMembersLoading = false;
+        draft.loadMembersError = action.error;
+        break;
+      //강퇴
+      case KICK_MEMBER_REQUEST:
+        draft.kickMemberLoading =true;
+        draft.kickMemberDone = false;
+        draft.kickMemberError = null;    
+        break;   
       case KICK_MEMBER_SUCCESS:
+        draft.kickMemberLoading = false;
         draft.members = draft.members.filter((m) => m.id !== action.data);
-        draft.isLoading = false;
+        draft.kickMemberDone=true
         break;
-
+      case KICK_MEMBER_FAILURE:
+        draft.kickMemberLoading =false;
+        draft.kickMemberError = action.error;
+        break;
+      //권한위임
+      case TRANSFER_OWNERSHIP_REQUEST:
+        draft.transferOwnershipLoading = true;
+        draft.transferOwnershipDone = false;
+        draft.transferOwnershipError = null;
+        break;       
       case TRANSFER_OWNERSHIP_SUCCESS:
+        draft.transferOwnershipLoading = false;
         draft.members = draft.members.map((m) => ({
           ...m,
           isLeader: m.id === action.data,
         }));
-        draft.isLoading = false;
+        draft.transferOwnershipDone=true;
         break;
-
-      case APPLY_GROUP_SUCCESS:
-        draft.isLoading = false;
-        break;
-
-      case LOAD_JOIN_REQUESTS_SUCCESS:
-        draft.joinRequests = action.data;
-        draft.isLoading = false;
-        break;
-
-      case APPROVE_JOIN_REQUEST:
-        draft.joinRequests = draft.joinRequests.filter((r) => r.id !== action.data);
-        break;
-
-      case REJECT_JOIN_REQUEST:
-        draft.joinRequests = draft.joinRequests.filter((r) => r.id !== action.data);
-        break;
-
-      case LOAD_GROUPS_FAILURE:
-      case CREATE_GROUP_FAILURE:
-      case UPDATE_GROUP_FAILURE:
-      case DELETE_GROUP_FAILURE:
-      case LOAD_MEMBERS_FAILURE:
-      case KICK_MEMBER_FAILURE:
       case TRANSFER_OWNERSHIP_FAILURE:
-      case APPLY_GROUP_FAILURE:
-      case LOAD_JOIN_REQUESTS_FAILURE:
-        draft.isLoading = false;
-        draft.error = action.error;
+        draft.transferOwnershipLoading = false;
+        draft.transferOwnershipError = action.error;
         break;
+      //가입신청
+      case APPLY_GROUP_REQUEST:
+        draft.applyGroupLoading = true;
+        draft.applyGroupDone = false;
+        draft.applyGroupError = null;       
+        break;
+      case APPLY_GROUP_SUCCESS:
+        draft.applyGroupLoading = false;
+        draft.applyGroupDone = true;
+        break;
+      case APPLY_GROUP_FAILURE:
+        draft.applyGroupLoading = false;
+        draft.applyGroupError = action.error;
+        break;
+      //가입신청 불러오기
+      case LOAD_JOIN_REQUESTS_REQUEST:
+        draft.loadJoinRequestsLoading = true;
+        draft.loadJoinRequestsDone = false;
+        draft.loadJoinRequestsError = null;  
+        break;
+      case LOAD_JOIN_REQUESTS_SUCCESS:
+        draft.loadJoinRequestsLoading = false;
+        draft.joinRequests = action.data;
+        draft.loadJoinRequestsDone=true;
+        break;
+      case LOAD_JOIN_REQUESTS_FAILURE:
+        draft.loadJoinRequestsLoading = false;
+        draft.loadJoinRequestsError = action.error;
+        break;
+
+      //가입 승인
+      case APPROVE_JOIN_REQUEST:
+        draft.approveJoinRequestLoading =true;
+        draft.approveJoinRequestDone = false;
+        draft.approveJoinRequestError = null;       
+        break;
+      case APPROVE_JOIN_SUCCESS:
+        draft.approveJoinRequestLoading =false;
+        draft.joinRequests = draft.joinRequests.filter((r) => r.id !== action.data);
+        draft.approveJoinRequestDone = true;
+        break;
+      case APPROVE_JOIN_FAILURE:
+        draft.approveJoinRequestLoading =false;        
+        draft.approveJoinRequestError = action.error;        
+        break;
+      //가입 거절
+      case REJECT_JOIN_REQUEST:
+        draft.rejectJoinRequestLoading =true;
+        draft.rejectJoinRequestDone =false;
+        draft.rejectJoinRequestError = null;
+        break;
+      case REJECT_JOIN_SUCCESS:
+        draft.rejectJoinRequestLoading =false;
+        draft.joinRequests = draft.joinRequests.filter((r) => r.id !== action.data);
+        draft.rejectJoinRequestDone =true;
+        break;
+      case REJECT_JOIN_FAILURE:
+        draft.rejectJoinRequestLoading =false;
+        draft.rejectJoinRequestError = action.error;
+        break;
+//--------------------------------------------------------------------------------//
 
       default:
         break;
--- front/sagas/group.js
+++ front/sagas/group.js
@@ -1,90 +1,58 @@
 import { all, call, fork, put, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
 import {
-  LOAD_GROUPS_REQUEST, LOAD_GROUPS_SUCCESS, LOAD_GROUPS_FAILURE,
-  CREATE_GROUP_REQUEST, CREATE_GROUP_SUCCESS, CREATE_GROUP_FAILURE,
-  UPDATE_GROUP_REQUEST, UPDATE_GROUP_SUCCESS, UPDATE_GROUP_FAILURE,
-  DELETE_GROUP_REQUEST, DELETE_GROUP_SUCCESS, DELETE_GROUP_FAILURE,
-  LOAD_MEMBERS_REQUEST, LOAD_MEMBERS_SUCCESS, LOAD_MEMBERS_FAILURE,
-  KICK_MEMBER_REQUEST, KICK_MEMBER_SUCCESS, KICK_MEMBER_FAILURE,
-  TRANSFER_OWNERSHIP_REQUEST, TRANSFER_OWNERSHIP_SUCCESS, TRANSFER_OWNERSHIP_FAILURE,
-  APPLY_GROUP_REQUEST, APPLY_GROUP_SUCCESS, APPLY_GROUP_FAILURE,
-  LOAD_JOIN_REQUESTS_REQUEST, LOAD_JOIN_REQUESTS_SUCCESS, LOAD_JOIN_REQUESTS_FAILURE,
-  APPROVE_JOIN_REQUEST, APPROVE_JOIN_REQUEST_SUCCESS, APPROVE_JOIN_REQUEST_FAILURE,
-  REJECT_JOIN_REQUEST, REJECT_JOIN_REQUEST_SUCCESS, REJECT_JOIN_REQUEST_FAILURE,
-} from '../reducers/group';
+  LOAD_GROUPS_REQUEST, LOAD_GROUPS_SUCCESS, LOAD_GROUPS_FAILURE, // 그룹 로드
+  CREATE_GROUP_REQUEST, CREATE_GROUP_SUCCESS, CREATE_GROUP_FAILURE, // 그룹 생성
+  UPDATE_GROUP_REQUEST, UPDATE_GROUP_SUCCESS, UPDATE_GROUP_FAILURE, // 그룹 수정
+  DELETE_GROUP_REQUEST, DELETE_GROUP_SUCCESS, DELETE_GROUP_FAILURE, // 그룹 삭제
+  LOAD_MEMBERS_REQUEST, LOAD_MEMBERS_SUCCESS, LOAD_MEMBERS_FAILURE, // 멤버 로드
+  KICK_MEMBER_REQUEST, KICK_MEMBER_SUCCESS, KICK_MEMBER_FAILURE, // 강퇴
+  TRANSFER_OWNERSHIP_REQUEST, TRANSFER_OWNERSHIP_SUCCESS, TRANSFER_OWNERSHIP_FAILURE, // 권한양도
+  APPLY_GROUP_REQUEST, APPLY_GROUP_SUCCESS, APPLY_GROUP_FAILURE, // 가입신청
+  LOAD_JOIN_REQUESTS_REQUEST, LOAD_JOIN_REQUESTS_SUCCESS, LOAD_JOIN_REQUESTS_FAILURE, // 신청현황
+  APPROVE_JOIN_REQUEST, APPROVE_JOIN_REQUEST_SUCCESS, APPROVE_JOIN_REQUEST_FAILURE, // 가입승인
+  REJECT_JOIN_REQUEST, REJECT_JOIN_REQUEST_SUCCESS, REJECT_JOIN_REQUEST_FAILURE, // 가입거절
+} from '@/reducers/group';
 
-// 1. 그룹 리스트 불러오기
-function loadGroupsAPI() {
-  return axios.get('/api/groups');
-}
-
+// 1. 그룹 로드
+function loadGroupsAPI() { return axios.get('/api/groups'); }
 function* loadGroups() {
   try {
     const result = yield call(loadGroupsAPI);
     yield put({ type: LOAD_GROUPS_SUCCESS, data: result.data });
-  } catch (err) {
-    yield put({ type: LOAD_GROUPS_FAILURE, error: err.response.data });
-  }
+  } catch (err) { yield put({ type: LOAD_GROUPS_FAILURE, error: err.response.data });}
 }
-
-function* watchLoadGroups() {
-  yield takeLatest(LOAD_GROUPS_REQUEST, loadGroups);
-}
+function* watchLoadGroups() { yield takeLatest(LOAD_GROUPS_REQUEST, loadGroups);}
 
 // 2. 그룹 생성
-function createGroupAPI(data) {
-  return axios.post('/api/groups', data);
-}
-
+function createGroupAPI(data) { return axios.post('/api/groups', data);}
 function* createGroup(action) {
   try {
     const result = yield call(createGroupAPI, action.data);
     yield put({ type: CREATE_GROUP_SUCCESS, data: result.data });
-  } catch (err) {
-    yield put({ type: CREATE_GROUP_FAILURE, error: err.response.data });
-  }
+  } catch (err) { yield put({ type: CREATE_GROUP_FAILURE, error: err.response.data }); }
 }
-
-function* watchCreateGroup() {
-  yield takeLatest(CREATE_GROUP_REQUEST, createGroup);
-}
+function* watchCreateGroup() {yield takeLatest(CREATE_GROUP_REQUEST, createGroup);}
 
 // 3. 그룹 수정
-function updateGroupAPI(data) {
-  return axios.patch(`/api/groups/${data.groupId}`, data);
-}
-
+function updateGroupAPI(data) { return axios.patch(`/api/groups/${data.groupId}`, data);}
 function* updateGroup(action) {
   try {
     const result = yield call(updateGroupAPI, action.data);
     yield put({ type: UPDATE_GROUP_SUCCESS, data: result.data });
-  } catch (err) {
-    yield put({ type: UPDATE_GROUP_FAILURE, error: err.response.data });
-  }
+  } catch (err) {yield put({ type: UPDATE_GROUP_FAILURE, error: err.response.data });}
 }
-
-function* watchUpdateGroup() {
-  yield takeLatest(UPDATE_GROUP_REQUEST, updateGroup);
-}
+function* watchUpdateGroup() {yield takeLatest(UPDATE_GROUP_REQUEST, updateGroup);}
 
 // 4. 그룹 삭제
-function deleteGroupAPI(groupId) {
-  return axios.delete(`/api/groups/${groupId}`);
-}
-
+function deleteGroupAPI(groupId) {return axios.delete(`/api/groups/${groupId}`);}
 function* deleteGroup(action) {
   try {
     yield call(deleteGroupAPI, action.data);
     yield put({ type: DELETE_GROUP_SUCCESS, data: action.data });
-  } catch (err) {
-    yield put({ type: DELETE_GROUP_FAILURE, error: err.response.data });
-  }
+  } catch (err) {yield put({ type: DELETE_GROUP_FAILURE, error: err.response.data });}
 }
-
-function* watchDeleteGroup() {
-  yield takeLatest(DELETE_GROUP_REQUEST, deleteGroup);
-}
+function* watchDeleteGroup() {yield takeLatest(DELETE_GROUP_REQUEST, deleteGroup);}
 
 // root saga
 export default function* groupSaga() {
