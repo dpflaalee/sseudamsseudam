ea9d81d0d2f9d80330103c93b8f4941f82e37afe
yj <during4277@gmail.com>
2025-06-09 00:28:04 +0900

    2025-06-08

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,6 +1,14 @@
 import React from "react";
 import LoginForm from "../components/user/LoginForm";
-const login = () => {
+
+import { useDispatch, useSelector } from 'react-redux';
+import Router from 'next/router';
+import { LOAD_MY_INFO_REQUEST, SIGN_UP_REQUEST } from '../reducers/user';
+import axios from 'axios';  
+import { END } from 'redux-saga'; 
+import wrapper from '../store/configureStore';
+
+const login = () => { 
     return (
         <div
              style={{
@@ -15,4 +23,20 @@ const login = () => {
     );
 };
 
+///////////////////////////////////////////////////////////
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => { 
+  //1. cookie 설정
+  const cookie = context.req ? context.req.headers.cookie : '';
+  axios.defaults.headers.Cookie = '';
+  
+  if (context.req  && cookie ) { axios.defaults.headers.Cookie = cookie;   }
+
+  //2. redux 액션
+  context.store.dispatch({ type:LOAD_MY_INFO_REQUEST});
+  //context.store.dispatch({ type: LOAD_POSTS_REQUEST });
+  context.store.dispatch(END);
+
+  await  context.store.sagaTask.toPromise();
+}); 
+///////////////////////////////////////////////////////////
 export default login;
