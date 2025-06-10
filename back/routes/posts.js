e4052d8a07c929ef0f3e52ff3263d4bde48c2370
yj <during4277@naver.com>
2025-06-10 14:23:17 +0900

    2025-06-10 routes posts 수정

--- back/routes/posts.js
+++ back/routes/posts.js
@@ -6,7 +6,38 @@ const { Op } = require('sequelize');
 router.get('/', async (req, res, next) => {
   try {
     const where = {};
-    if(req.query.userId){
+    if( parseInt(req.query.lastId, 10) ) { where.id={ [Op.lt]: parseInt(req.query.lastId, 10) } }
+    const posts = await Post.findAll({
+      where,
+      limit: 10,
+      order: [
+        ['createdAt', 'DESC'],
+        [Comment, 'createdAt', 'DESC']
+      ],
+      include: [
+        { model: User, attributes: ['id', 'nickname', 'isAdmin'] },
+        { model: Image },
+        { model: Comment, include: [{ model: User, attributes: ['id', 'nickname', 'isAdmin'] }] },
+        { model: User, as: 'Likers', attributes: ['id'] },
+        { model: Post, as: 'Retweet', include: [{ model: User, attributes: ['id', 'nickname'] }, { model: Image }] },
+        { model: OpenScope }
+      ]
+    });
+    res.status(200).json(posts);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+router.get('/myPost', async (req, res, next) => {
+  try {
+    const where = {};
+    console.log('req.query.userId',req.query.userId);
+    console.log('req.query.lastId',req.query.lastId);
+    const userId = req.query.userId;
+    
+    if(userId !== 'undefined'){
+    //if(req.query.userId){
       where.UserId = req.query.userId;
     }
     if( parseInt(req.query.lastId, 10) ) { where.id={ [Op.lt]: parseInt(req.query.lastId, 10) } }
