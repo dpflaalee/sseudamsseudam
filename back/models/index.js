3589e58c676d81bd2c3dd2d5b9adb220ac22ff74
dpflaalee <dpflaalee@naver.com>
2025-07-25 17:09:08 +0900

    Update index.js

--- back/models/index.js
+++ back/models/index.js
@@ -2,53 +2,46 @@ const Sequelize = require('sequelize');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
-const db = {};  // 객체 생성하여 객체저장공간 만들기
+const db = {}; 
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
-//db, username, password, cinfig 설정정보 이용하여 인스턴스 생성
 
-//#모델정의
-
-db.User = require('./user')(sequelize, Sequelize);  // 모듈연결   sequelize 실행
-db.Post = require('./post')(sequelize, Sequelize);  // 모듈연결   sequelize 실행
-db.Prize = require('./prize')(sequelize, Sequelize);  // 모듈연결   sequelize 실행
-db.Image = require('./image')(sequelize, Sequelize);  // 모듈연결   sequelize 실행
-db.Hashtag = require('./hashtag')(sequelize, Sequelize);  // 모듈연결   sequelize 실행
-db.Comment = require('./comment')(sequelize, Sequelize);  // 모듈연결   sequelize 실행
-db.Animal = require('./animal')(sequelize, Sequelize);  // 모듈연결   sequelize 실행
-db.Category = require('./category')(sequelize, Sequelize);  // 모듈연결   sequelize 실행
-db.OpenScope = require('./openScope')(sequelize, Sequelize);  // 모듈연결   sequelize 실행
-db.Place = require('./place')(sequelize, Sequelize);  // 모듈연결   sequelize 실행
-db.Group = require('./group')(sequelize, Sequelize);  // 모듈연결   sequelize 실행
+//모델정의
+db.User = require('./user')(sequelize, Sequelize);
+db.Post = require('./post')(sequelize, Sequelize);
+db.Prize = require('./prize')(sequelize, Sequelize); 
+db.Image = require('./image')(sequelize, Sequelize); 
+db.Hashtag = require('./hashtag')(sequelize, Sequelize);  
+db.Comment = require('./comment')(sequelize, Sequelize); 
+db.Animal = require('./animal')(sequelize, Sequelize); 
+db.Category = require('./category')(sequelize, Sequelize); 
+db.OpenScope = require('./openScope')(sequelize, Sequelize); 
+db.Place = require('./place')(sequelize, Sequelize); 
+db.Group = require('./group')(sequelize, Sequelize); 
 db.GroupMember = require('./groupmember')(sequelize, Sequelize);
 db.GroupRequest = require('./groupRequest')(sequelize, Sequelize);
-db.Complain = require('./complain')(sequelize, Sequelize);  // 모듈연결   sequelize 실행
-db.Notification = require('./notification')(sequelize, Sequelize);  // 모듈연결   sequelize 실행
+db.Complain = require('./complain')(sequelize, Sequelize); 
+db.Notification = require('./notification')(sequelize, Sequelize); 
 db.NotificationSetting = require('./notificaionSetting')(sequelize, Sequelize);
-db.Calendar = require('./calendar')(sequelize, Sequelize);  // 모듈연결   sequelize 실행
+db.Calendar = require('./calendar')(sequelize, Sequelize);
 
-db.Chatting = require('./chatting')(sequelize, Sequelize);  // 모듈연결   sequelize 실행
-db.ChattingRoom = require('./chattingRoom')(sequelize, Sequelize);  // 모듈연결   sequelize 실행
-db.ChattingImage = require('./chattingImage')(sequelize, Sequelize);  // 모듈연결   sequelize 실행
-db.ChattingMember = require('./chattingMember')(sequelize, Sequelize);  // 모듈연결   sequelize 실행
-db.Blacklist = require('./blackList')(sequelize, Sequelize);  // 모듈연결   sequelize 실행
-db.MyPrize = require('./myPrize')(sequelize, Sequelize);  // 모듈연결   sequelize 실행
+db.Chatting = require('./chatting')(sequelize, Sequelize);
+db.ChattingRoom = require('./chattingRoom')(sequelize, Sequelize);
+db.ChattingImage = require('./chattingImage')(sequelize, Sequelize); 
+db.ChattingMember = require('./chattingMember')(sequelize, Sequelize); 
+db.Blacklist = require('./blackList')(sequelize, Sequelize);
+db.MyPrize = require('./myPrize')(sequelize, Sequelize);  
 db.IssuedRandomBox = require('./issuedrandombox')(sequelize, Sequelize);
-db.UserProfileImage = require('./userProfileImage')(sequelize, Sequelize);  // 모듈연결   sequelize 실행
+db.UserProfileImage = require('./userProfileImage')(sequelize, Sequelize); 
 
-
-
-
-//#모델 관계설정
+//모델 관계설정
 Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {  // model안에  modelName associate 가 있다면 
-    db[modelName].associate(db);  // associate 실행
+  if (db[modelName].associate) { 
+    db[modelName].associate(db); 
   }
 });
 
+db.sequelize = sequelize; 
+db.Sequelize = Sequelize;
 
-
-db.sequelize = sequelize; //인스턴스
-db.Sequelize = Sequelize; //라이브러리 db객체에 저장
-
-module.exports = db; // 타 파일에서도 db 사용가능하게
+module.exports = db;
