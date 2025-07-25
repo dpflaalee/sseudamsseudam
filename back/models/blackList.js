519be6c8fc1bb8869558f0e97e1f92a0a8459464
dpflaalee <dpflaalee@naver.com>
2025-07-25 17:02:11 +0900

    Update blackList.js

--- back/models/blackList.js
+++ back/models/blackList.js
@@ -1,6 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
   const Blacklist = sequelize.define('Blacklist', {
-    // 필요 시 필드 추가 가능 (예: 차단 시각 등)
   }, {
     charset: 'utf8',
     collate: 'utf8_general_ci',
@@ -8,12 +7,12 @@ module.exports = (sequelize, DataTypes) => {
 
   Blacklist.associate = (db) => {
     Blacklist.belongsTo(db.User, {
-      as: 'Blocked', // 차단 당한 사람
+      as: 'Blocked',
       foreignKey: 'BlockedId',
     });
 
     Blacklist.belongsTo(db.User, {
-      as: 'Blocking', // 차단한 사람
+      as: 'Blocking', 
       foreignKey: 'BlockingId',
     });
   };
