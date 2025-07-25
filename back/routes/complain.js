718b5fe451d237fbfe583a1246e604e0525e94ea
dpflaalee <dpflaalee@naver.com>
2025-07-25 17:44:46 +0900

    Update complain.js

--- back/routes/complain.js
+++ back/routes/complain.js
@@ -4,7 +4,6 @@ const { Post, User, Comment, Complain, Image, RandomBox } = require('../models')
 const { Op } = require('sequelize');
 const TARGET_TYPE = require('../../shared/constants/TARGET_TYPE');
 
-// 신고하기
 router.post('/', async (req, res, next) => {
     try {
         const complain = await Complain.create({
@@ -14,20 +13,17 @@ router.post('/', async (req, res, next) => {
             ReporterId: req.body.reporterId,
         });
 
-        res.status(201).json(complain); // 원하는 형식으로 가공 가능
+        res.status(201).json(complain); 
     } catch (err) {
-        console.error('🚨 ComplainRouter 에러: ', err);
         res.status(500).send('신고 대상 검색 실패');
     }
 });
 
-// 신고 내용 블라인드 처리
 router.patch('/blind', async (req, res, next) => {
     try {
         await Complain.update({ isBlind: true }, { where: { targetId: req.body.targetId } });
         res.status(200).json({ message: '블라인드 처리 완료' });
     } catch (err) {
-        console.error('🚨 블라인드 처리 실패:', err);
         res.status(500).send('실패');
     }
 });
