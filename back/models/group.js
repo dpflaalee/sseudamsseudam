b03f3d836286d204423fe8e936f949a9bc56c7d2
dpflaalee <dpflaalee@naver.com>
2025-07-25 17:06:58 +0900

    Update group.js

--- back/models/group.js
+++ back/models/group.js
@@ -22,10 +22,3 @@ module.exports = (sequelize, DataTypes) => {
   return Group;
 };
 
-/*
-그룹은 하나의 공개범위를 가짐 하나의 공개범위는 많은 그룹을 가짐 다대1 belongsTo
-그룹은 많은 유저를 가질 수 있음 - 유저는 많은 그룹을 가질 수 있음 다대다 belongsToMany
-그룹은 많은 카테고리를 가질 수 있음 카테고리는 많은 그룹을 가질 수있음 다대다 belongsToMany
-그룹은 많은 포스트를 가질 수 있음 1대다 hasmany
-*/
-
