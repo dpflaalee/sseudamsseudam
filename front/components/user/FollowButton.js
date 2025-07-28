f4dfd5786a23b3eae9b1d53e265f6eb28bb03f16
dpflaalee <dpflaalee@naver.com>
2025-07-28 10:26:44 +0900

    Update FollowButton.js

--- front/components/user/FollowButton.js
+++ front/components/user/FollowButton.js
@@ -1,34 +1,28 @@
-import React, { useCallback } from "react"; //부품객체
-import { Button } from 'antd'; // 디자인
-import PropTypes from "prop-types"; //넘겨받은 파라미터 테스트
-import { useSelector, useDispatch } from "react-redux"; //중앙저장소 부품
-import { FOLLOW_REQUEST, UNFOLLOW_REQUEST, UNFOLLOW_SUCCESS } from "@/reducers/user"; //액션타입
+import React, { useCallback } from "react"; 
+import { Button } from 'antd';
+import PropTypes from "prop-types"; 
+import { useSelector, useDispatch } from "react-redux"; 
+import { FOLLOW_REQUEST, UNFOLLOW_REQUEST, UNFOLLOW_SUCCESS } from "@/reducers/user"; 
 
 
 const FollowButton = ({ postUser, setPostUser, currentUserId }) => {
   /////////////////////////////////////code
   const dispatch = useDispatch();
   const { user, followLoading, unFollowLoading } = useSelector(state => state.user);
-  //팔로잉하는 사람들 목록중에 아이디가 있니? - 팔로잉여부
   const isFollowing = user?.Followings.some((v) => v.id == postUser.id);
-//const isFollowing = !!postUser?.Followers?.find(f => f.id === currentUserId);
-  console.log("postUser 객체 구조:", postUser);
-  console.log("currentUserIdcurrentUserId", currentUserId);
-
+  
   const onClickFollow = useCallback(() => {
 
-    if (isFollowing) { // 팔로잉 - 내친구 - 언팔로우
+    if (isFollowing) {
       dispatch({
         type: UNFOLLOW_REQUEST,
         data: postUser.id
       })
-      // 상태 업데이트
-      console.log(setPostUser);
       setPostUser((prev) => ({
         ...prev,
         Followers: prev.Followers.filter((f) => f.id !== currentUserId),
       }));
-    } else { // 팔로우
+    } else { 
       dispatch({
         type: FOLLOW_REQUEST,
         data: postUser.id,
@@ -37,7 +31,6 @@ const FollowButton = ({ postUser, setPostUser, currentUserId }) => {
           ReceiverId: postUser?.id,
         }
       })
-      // 상태 업데이트
       setPostUser((prev) => ({
         ...prev,
         Followers: [...prev.Followers, { id: currentUserId }],
