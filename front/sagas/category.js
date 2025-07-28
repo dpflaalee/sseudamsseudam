28be17617b98b960603ffd6937df0034bbd4db3c
dpflaalee <dpflaalee@naver.com>
2025-07-28 10:47:48 +0900

    Update category.js

--- front/sagas/category.js
+++ front/sagas/category.js
@@ -12,12 +12,10 @@ import {
     EDIT_CATEGORY_FAILURE,
 } from '../reducers/category';
 
-// 카테고리 목록 로드 API
 function loadCategoriesAPI() {
     return axios.get('/category');
 }
 
-// 카테고리 목록 로드 작업 (saga)
 function* loadCategories() {
     try {
         const result = yield call(loadCategoriesAPI);
@@ -33,12 +31,10 @@ function* loadCategories() {
     }
 }
 
-// 카테고리 추가 API
 function addCategoryAPI(data) {
     return axios.post('/category', data);
 }
 
-// 카테고리 추가 작업 (saga)
 function* addCategory(action) {
     try {
         const result = yield call(addCategoryAPI, action.data);
@@ -54,12 +50,10 @@ function* addCategory(action) {
     }
 }
 
-// 카테고리 수정 API (PATCH 요청)
 function editCategoryAPI(data) {
     return axios.patch(`/category/${data.id}`, data);
 }
 
-// 카테고리 수정 작업 (saga)
 function* editCategory(action) {
     try {
         const result = yield call(editCategoryAPI, action.data);
@@ -75,22 +69,18 @@ function* editCategory(action) {
     }
 }
 
-// 카테고리 목록 로드 watcher
 function* watchLoadCategories() {
     yield takeLatest(LOAD_CATEGORIES_REQUEST, loadCategories);
 }
 
-// 카테고리 추가 watcher
 function* watchAddCategory() {
     yield takeLatest(ADD_CATEGORY_REQUEST, addCategory);
 }
 
-// 카테고리 수정 watcher
 function* watchEditCategory() {
     yield takeLatest(EDIT_CATEGORY_REQUEST, editCategory);
 }
 
-// 모든 사가를 묶어서 export
 export default function* categorySaga() {
     yield all([
         fork(watchLoadCategories),
